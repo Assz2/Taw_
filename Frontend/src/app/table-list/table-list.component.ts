968b36f9c957090c9bd8cf6f282f170fa5fd9203
Assz2 <alessandro.sozzo03@gmail.com>
2023-07-12 17:51:42 +0200

    table-list html

--- Frontend/src/app/table-list/table-list.component.ts
+++ Frontend/src/app/table-list/table-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TableHttpService, Table } from '../table-http.service';
+import { UserHttpService } from '../user-http.service';
 
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.css']
 })
-export class TableListComponent {
+export class TableListComponent implements OnInit {
+  
+  public tables: Table[] = [];
+  constructor(private router: Router, private ts: TableHttpService, public us: UserHttpService) { }
 
+  ngOnInit(){
+    
+  }
+
+  getTables(filter?: string){
+    this.ts.getTables(filter).subscribe({
+      next: (data) => {
+        console.log("Received tables: " + JSON.stringify(data));
+      },
+      error: (err) => {
+        console.log("Error: " + JSON.stringify(err));
+        this.logout();
+      }
+    });
+  }
+
+  logout(){
+    this.us.logout();
+    this.router.navigate(['/']);
+  }
 }
